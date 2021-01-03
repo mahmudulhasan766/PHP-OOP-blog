d61c90472552f2dbff65e20dd1c2503bb1dd6909
HaSaN <mahamudul951@gmail.com>
2021-01-03 14:52:40 +0600

    composer and add templeat

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e6008e9d3bad2c820f2ae4cefd9ae59b539b6895',
+    'name' => 'php/blog',
+  ),
+  'versions' => 
+  array (
+    'php/blog' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e6008e9d3bad2c820f2ae4cefd9ae59b539b6895',
+    ),
+  ),
+);
